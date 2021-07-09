79abd7038f77eae09c40f25dffd75ce7864c03be
ouzmargof <55629733+ouzmargof@users.noreply.github.com>
2021-07-09 17:49:41 +0200

    Move webapps/

---
+++ webapps/serial-communication-diagram-online/precache-manifest.4c350219e19260bf83e19d04f801cf58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe31586bc4520d8f89c4e4c7f7edae82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "565ef48f72f6ecc7bb73",
+    "url": "./static/css/main.8defce25.chunk.css"
+  },
+  {
+    "revision": "db494fad8e8f66a4a100",
+    "url": "./static/js/2.4e349af8.chunk.js"
+  },
+  {
+    "revision": "28bcb252a64156eb2fda27866b95e924",
+    "url": "./static/js/2.4e349af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565ef48f72f6ecc7bb73",
+    "url": "./static/js/main.b04925e7.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
