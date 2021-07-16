1bbe5af040e364452e60c2e14a3ab7e7b657df29
ouzmargof <55629733+ouzmargof@users.noreply.github.com>
2021-07-16 09:42:17 +0200

    Add webapps/

---
+++ webapps/control-theory-plot-online/precache-manifest.b045c471e3d5e9d5e6c8bce0057701d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24149afc7ffe9f417d1e86719770dd1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10f9f124e89708e0d62c",
+    "url": "./static/css/main.eae61196.chunk.css"
+  },
+  {
+    "revision": "b2126a5ae481cee22794",
+    "url": "./static/js/2.1c91af15.chunk.js"
+  },
+  {
+    "revision": "28bcb252a64156eb2fda27866b95e924",
+    "url": "./static/js/2.1c91af15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f9f124e89708e0d62c",
+    "url": "./static/js/main.8cd21891.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
